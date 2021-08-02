ad2595acff4d1f223ef933a2a9e15822dd820870
kamau <fkmuiruri8@gmail.com>
2021-08-02 13:09:29 +0300

    Add hanoi function

--- recursion.py
+++ recursion.py
@@ -2,3 +2,13 @@ def move(f,t):
     print(f' Move from {f} to {t}')
 
 # move('A','B')
+def hanoi(n,f,v,t):
+    if n==0:
+        pass
+    else:
+        hanoi(n-1,f,t,v)
+        move(f,t)
+        hanoi(n-1,v,f,t)
+
+
+hanoi(3,'A','B','C')
